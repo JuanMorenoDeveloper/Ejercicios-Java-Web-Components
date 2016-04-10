4a5d1a1df996c6ec91371a2fd2cc9e119a4c351a
jmoreno <earth001@gmail.com>
2016-04-09 22:05:21 -0300

    Se agrega ejemplo de de transferencia de atributos (Request, Session y Application scope)

--- example-servlet-attribute-scopes/src/main/java/com/proitc/servlet/ServletAttributeScopesReceived.java
+++ example-servlet-attribute-scopes/src/main/java/com/proitc/servlet/ServletAttributeScopesReceived.java
@@ -1,5 +1,7 @@
 package com.proitc.servlet;
 
+import static java.util.Objects.nonNull;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -8,6 +10,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet(urlPatterns = { "/attributeScopesReceived" },
 name = "ServletAttributeScopesReceived",
@@ -19,8 +22,19 @@ public class ServletAttributeScopesReceived extends HttpServlet {
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter writer = response.getWriter();
+		// Leyendo un valor vía request scope
 		String attrib1 = (String) request.getAttribute("attrib1");
+		// Leyendo un valor vía session
+		String attrib2 = null;
+		HttpSession session = request.getSession(false);
+		if (nonNull(session)) {
+			attrib2 = (String) session.getAttribute("attrib2");
+		}
+		// Leyendo atributo vía application scope
+		String attrib3 = (String) getServletContext().getAttribute("attrib3");
 		writer.print("ServletAttributeScopesReceived<br>" +
-				"Received: attrib1=" + attrib1 + "<br>");
+				"Received: attrib1=" + attrib1 + "<br>" +
+				"Received: attrib2=" + attrib2 + "<br>" +
+				"Received: attrib3=" + attrib3 + "<br>");
 	}
 }
--- example-servlet-attribute-scopes/src/main/java/com/proitc/servlet/ServletAttributeScopesSender.java
+++ example-servlet-attribute-scopes/src/main/java/com/proitc/servlet/ServletAttributeScopesSender.java
@@ -1,5 +1,7 @@
 package com.proitc.servlet;
 
+import static java.util.Objects.nonNull;
+
 import java.io.IOException;
 import java.util.logging.Logger;
 
@@ -8,6 +10,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet(urlPatterns = { "/attributeScopesSender" },
 name = "ServletAttributeScopesSender",
@@ -18,8 +21,18 @@ public class ServletAttributeScopesSender extends HttpServlet {
 
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Enviando un objeto vía request scope
 		request.setAttribute("attrib1", "value1");
 		log.info("ServletAttributeScopesSender: Sending attrib1=" + request.getAttribute("attrib1"));
+		// Enviando un valor vía HttpSession
+		HttpSession session = request.getSession();
+		if (nonNull(session)) {
+			session.setAttribute("attrib2", "value2");
+			log.info("ServletAttributeScopesSender: Sending attrib2=" + session.getAttribute("attrib2"));
+		}
+		// Enviando atributo vía application scope
+		getServletContext().setAttribute("attrib3", "value3");
+		log.info("ServletAttributeScopesSender: Sending attrib3=" + getServletContext().getAttribute("attrib3"));
 		request.getRequestDispatcher("attributeScopesReceived").forward(request, response);
 	}
 }
